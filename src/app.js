fa42f9c44ad5aed841e5560a8db80c958d6d5e7f
LaryssaGabi <lary.gabriela85@gmail.com>
2024-11-04 23:50:26 -0300

    adicionado o corsOptions

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import './database/index.js';
 
+const corsOptions = {
+    origin: 'https://dev-burguer-web.vercel.app',
+    credential: true,
+}
+
 // Obter o __dirname no contexto de ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = resolve(__filename, '..');
@@ -13,7 +18,7 @@ class App {
     constructor() {
         this.app = express();
 
-        this.app.use(cors());
+        this.app.use(cors(corsOptions));
         this.middlewares();
         this.routes();
     }
