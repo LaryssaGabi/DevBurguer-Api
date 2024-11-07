90d318d08bd1a1d3017ad0dc96056d66a573e623
LaryssaGabi <lary.gabriela85@gmail.com>
2024-11-06 23:03:02 -0300

    ajuste na por app.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import App from './app.js'
+import app from './app.js'
 
 const port = 3001;
-App.listen(port, () => console.log(`Server is running on port ${port}!`));
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
